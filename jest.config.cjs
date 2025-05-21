51f1201443a3ffab2930487ee61f4e2a5d57f025
shuyi56 <shuyi56@gmail.com>
2025-05-21 12:57:13 -0400

    setup testing lib

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testMatch: ["**/?(*.)+(test).[tj]s?(x)"],
+};
